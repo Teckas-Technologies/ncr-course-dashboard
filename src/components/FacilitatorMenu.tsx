ac00c0fc5ed6c2556c66f46f84e1d0222d842c2d
SharmilaBlessyS <sharmila@kalakendradao.org>
2024-07-23 21:29:09 +0530

    Added active menu bar

--- src/components/FacilitatorMenu.tsx
+++ src/components/FacilitatorMenu.tsx
@@ -1,9 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { ArrowRightIcon } from "lucide-react";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
 import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-
 
 const menuList = [
     {
@@ -12,37 +11,48 @@ const menuList = [
     {
         name: "Student List",
     }
-]
+];
 
-export default function FacilitatorMenu({setPageComponent} : any) {
-    
+export default function FacilitatorMenu({ setPageComponent }: any) {
+    const [activeMenu, setActiveMenu] = useState<string>(menuList[0].name);
+
+    const handleMenuClick = (menuName: string) => {
+        setActiveMenu(menuName);
+        setPageComponent(menuName);
+    };
+
     return (
         <>
-        <div className="facilitator-menu">
-            <Card>
-                <CardHeader className="border-b-2 mb-5">
-                    <CardTitle className="flex flex-row items-center gap-3">
-                        <Avatar>
-                            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                            <AvatarFallback>CN</AvatarFallback>
-                        </Avatar>
-                        <div className="account flex flex-col gap-1">
-                            <h3>My Account </h3>
-                            {/* <p className="font-normal">id : RC5421</p> */}
-                            <CardDescription>id : RC5421</CardDescription>
-                        </div>
-                    </CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <ul className="facilitator-menu-list flex-col flex gap-4 pr-2">
-                    {menuList.map((menu:any, i:any) => (
-                        <li onClick={() => setPageComponent(menu.name)} key={i} className="flex justify-between align-center hover:pr-4"><h2>{menu.name}</h2> <ArrowRightIcon className="menu-icon" /></li>
-                    ))}
-                    </ul>
-                </CardContent>
-            </Card>
-        </div>
+            <div className="facilitator-menu">
+                <Card>
+                    <CardHeader className="border-b-2 mb-5">
+                        <CardTitle className="flex flex-row items-center gap-3">
+                            <Avatar>
+                                <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                                <AvatarFallback>CN</AvatarFallback>
+                            </Avatar>
+                            <div className="account flex flex-col gap-1">
+                                <h3>My Account</h3>
+                                <CardDescription>id : RC5421</CardDescription>
+                            </div>
+                        </CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <ul className="facilitator-menu-list flex-col flex gap-4 pr-2">
+                            {menuList.map((menu: any, i: any) => (
+                                <li
+                                    onClick={() => handleMenuClick(menu.name)}
+                                    key={i}
+                                    className={`flex justify-between align-center  ${activeMenu === menu.name ? 'active' : ''}`}
+                                >
+                                    <h2>{menu.name}</h2> 
+                                    <ArrowRightIcon className={`menu-icon ${activeMenu === menu.name ? 'active-icon' : ''}`} />
+                                </li>
+                            ))}
+                        </ul>
+                    </CardContent>
+                </Card>
+            </div>
         </>
-    )
-
+    );
 }
