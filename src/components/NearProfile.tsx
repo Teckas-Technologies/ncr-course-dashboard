0705e5620d6c87e0fe5d4db087fd60dbbf7c2119
SharmilaBlessyS <sharmila@kalakendradao.org>
2024-08-08 17:43:28 +0530

    Toast notification added for copy icon

--- src/components/NearProfile.tsx
+++ src/components/NearProfile.tsx
@@ -7,7 +7,7 @@ interface BackgroundShow {
 }
 export default function NearProfile({ setShowDetails }: BackgroundShow) {
   const [showProfileDetails, setshowProfileDetails] = useState(false);
-  const [copied, setCopied] = useState(false);
+  const [showToast, setShowToast] = useState(false);
 
   const handleEditClick = () => {
     setshowProfileDetails(true);
@@ -20,10 +20,10 @@ export default function NearProfile({ setShowDetails }: BackgroundShow) {
       navigator.clipboard
         .writeText(textToCopy)
         .then(() => {
-          setCopied(true);
+          setShowToast(true);
           setTimeout(() => {
-            setCopied(false);
-          }, 500);
+            setShowToast(false);
+          }, 2000); // Hide the toast after 2 seconds
         })
         .catch((error) => {
           console.error("Failed to copy:", error);
@@ -37,27 +37,29 @@ export default function NearProfile({ setShowDetails }: BackgroundShow) {
         <NearProfileDetails setshowProfileDetails={setshowProfileDetails} />
       ) : (
         <div className="profile-container" style={{ position: "relative" }}>
-          <div className="icon-container">
-            <Pencil className="top-right-icon" onClick={handleEditClick} />
-          </div>
-          <div className="profile-info">
-            <img
-              className="profile-image"
-              src="https://i.pinimg.com/236x/da/fd/f2/dafdf25168edcb2f0e1d8702797946cc.jpg"
-              alt="Profile"
-            />
-          </div>
-          <div className="profile-details">
-            <h2>Jessica Jones</h2>
-            <div className="acc-id-container">
-              <p className="acc_id">calista_molten.testnet</p>
-              <Copy
-                className={`copy-icon ${copied ? "copied" : ""}`}
-                onClick={handleCopyClick}
-              />
+        <div className="icon-container">
+          <Pencil className="top-right-icon" onClick={handleEditClick} />
+        </div>
+        <div className="profile-info">
+          <img
+            className="profile-image"
+            src="https://i.pinimg.com/236x/da/fd/f2/dafdf25168edcb2f0e1d8702797946cc.jpg"
+            alt="Profile"
+          />
+        </div>
+        <div className="profile-details">
+          <h2>Jessica Jones</h2>
+          <div className="acc-id-container">
+            <p className="acc_id">calista_molten.testnet</p>
+            <div className="copy_id" style={{ position: "relative" }}>
+              <Copy className="copy-icon" onClick={handleCopyClick} />
+              <div className={`toast-container ${showToast ? "show" : ""}`}>
+                Copied
+              </div>
             </div>
           </div>
         </div>
+      </div>
       )}
     </>
   );
